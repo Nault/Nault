eadea37aaa1fce6747e8158d7207a8f777b35755
Andrew Steele <cronoh@gmail.com>
2018-02-28 02:23:49 -0800

    Remove unused thread js file

--- src/assets/lib/pow/thread.js
+++ src/assets/lib/pow/thread.js
@@ -3,22 +3,18 @@ self.importScripts('pow.js');
 var ready = false;
 
 Module['onRuntimeInitialized'] = function() {
-    postMessage('ready');
-}
+  postMessage('ready');
+};
 
-onmessage = function(ev)
-{
-    var PoW = Module.cwrap("launchPoW", 'string', ['string']);
-    var hash = ev.data;
-	//let generate = Module.ccall("launchPoW", 'string', ['string'], hash);
-	let generate = PoW(hash);
-	
-	if (generate != "0000000000000000") {
-	    console.log(generate +" found");
-		postMessage(generate); // Worker return
-	}
-	else
-	{
-	    postMessage(false);
-	}
-}
+onmessage = function(ev) {
+  var PoW = Module.cwrap("launchPoW", 'string', ['string']);
+  var hash = ev.data;
+  //let generate = Module.ccall("launchPoW", 'string', ['string'], hash);
+  var generate = PoW(hash);
+
+  if (generate != "0000000000000000") {
+    postMessage(generate); // Worker return
+  } else {
+    postMessage(false);
+  }
+};
