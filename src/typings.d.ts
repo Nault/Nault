01813d4575426992b2e8e863273fb7fd6fa54e10
Joohansson <d84@outlook.com>
2020-07-03 08:40:37 +0200

    remove duplicate workertype

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,12 +1,5 @@
 /* SystemJS module definition */
 declare var module: NodeModule;
-declare module 'worker-loader!*' {
-  class WebpackWorker extends Worker {
-    constructor();
-  }
-
-  export default WebpackWorker;
-}
 interface NodeModule {
   id: string;
 }
