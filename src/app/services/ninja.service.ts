1a7bb1505f519fe4510149f6c93c0b15757fb001
Tobias Palmer <tobiaspalmer93@gmail.com>
2020-07-07 22:23:32 +0200

    first ninja service

---
+++ src/app/services/ninja.service.ts
@@ -0,0 +1,70 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { NotificationService } from './notification.service';
+import { UtilService } from './util.service';
+
+@Injectable()
+export class NinjaService {
+
+  // URL to Ninja API
+  ninjaUrl = 'https://mynano.ninja/api/';
+
+  // null - loading, false - offline, true - online
+  status = null;
+
+  constructor(private http: HttpClient, private notifications: NotificationService, private util: UtilService) { }
+
+  private async request(action): Promise<any> {
+    return await this.http.get(this.ninjaUrl + action).toPromise()
+      .then(res => {
+        return res;
+      })
+      .catch(err => {
+        console.error(err);
+        return;
+      });
+  }
+
+  private randomizeByScore(replist: any) {
+
+    let scores = {};
+    let newlist = [];
+
+    for (const account of replist) {
+      scores[account.score] = scores[account.score] || [];
+      scores[account.score].push(account);
+    }
+
+    for (const score in scores) {
+      if (scores.hasOwnProperty(score)) {
+        let accounts = scores[score];
+        accounts = this.util.array.shuffle(accounts);
+
+        for (const account of accounts) {
+          newlist.unshift(account);
+        }
+      }
+    }
+
+    return newlist;
+  }
+
+  async verified(): Promise<any> {
+    return await this.request('accounts/verified');
+  }
+
+  async verifiedRandomized(): Promise<any> {
+    const replist = await this.verified();
+    return this.randomizeByScore(replist);
+  }
+
+  async getSuggestedRep(): Promise<any> {
+    const replist = await this.verifiedRandomized();
+    return replist[0].account;
+  }
+
+  async getAccount(account: string): Promise<any> {
+    return await this.request('accounts/' + account);
+  }
+
+}
