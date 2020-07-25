7114ba1a580159f4ab484ed2fb9cccbaa7cbc717
Tobias Palmer <tobiaspalmer93@gmail.com>
2020-07-25 21:09:22 +0200

    env

--- src/environments/environment.desktop.ts
+++ src/environments/environment.desktop.ts
@@ -1,4 +1,13 @@
 export const environment = {
   production: true,
   desktop: true,
+  'currency': {
+    'name': 'Nano',
+    'shortName': 'NANO',
+    'precision': 30,
+    'maxSupply': 133248289,
+    'prefixes': ['xrb', 'nano'],
+    'qrPrefix': 'xrb',
+    'supportsMyNanoNinja': true
+  },
 };
