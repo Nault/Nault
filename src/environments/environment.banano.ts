687c992fac8e7ae5a252f799616ac8d5af74e136
Tobias Palmer <tobiaspalmer93@gmail.com>
2020-07-25 18:33:55 +0200

    first banano

---
+++ src/environments/environment.banano.ts
@@ -0,0 +1,18 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  desktop: false,
+  'currency': {
+    'name': 'Banano',
+    'shortName': 'BAN',
+    'precision': 29,
+    'maxSupply': 3402823669.2,
+    'prefixes': ['ban'],
+    'qrPrefix': 'ban',
+    'supportsMyNanoNinja': false
+  },
+};
