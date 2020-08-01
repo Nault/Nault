7b8ee13578e98c68f5d4d0d004953990c45232eb
Aleksander Rem <alexx87@gmail.com>
2020-08-01 17:47:11 +0200

    Fixed deprecated currency symbol usage

--- src/app/pipes/currency-symbol.pipe.ts
+++ src/app/pipes/currency-symbol.pipe.ts
@@ -8,7 +8,7 @@ export class CurrencySymbolPipe extends CurrencyPipe implements PipeTransform {
 
   // This pipe simply shows the currency symbol ($, BTC, etc) and removes any numeric values
   transform(value: any, args?: any): any {
-    let currency = super.transform(0, value, true, '1.0-2');
+    let currency = super.transform(0, value, 'symbol', '1.0-2');
     return currency.replace(/[0-9]/g, '');
   }
 
