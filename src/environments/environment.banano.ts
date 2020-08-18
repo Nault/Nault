95557b1d54440e9329bf74b394f3f6659fbe80c5
sebrock <156824+sebrock@users.noreply.github.com>
2020-08-18 16:52:15 +0200

    added default reps

--- src/environments/environment.banano.ts
+++ src/environments/environment.banano.ts
@@ -37,5 +37,56 @@ export const environment = {
     'ban_1cake36ua5aqcq1c5i3dg7k8xtosw7r9r7qbbf5j15sk75csp9okesz87nfn', // Official Rep - Cake
     'ban_1fomoz167m7o38gw4rzt7hz67oq6itejpt4yocrfywujbpatd711cjew8gjj', // Official Rep - FOMO
   ],
-  defaultRepresentatives: []
+  defaultRepresentatives = [
+    {
+      id: 'ban_1fomoz167m7o38gw4rzt7hz67oq6itejpt4yocrfywujbpatd711cjew8gjj',
+      name: 'Official Fomo Rep',
+      warn: true,
+    },
+    {
+      id: 'ban_1cake36ua5aqcq1c5i3dg7k8xtosw7r9r7qbbf5j15sk75csp9okesz87nfn',
+      name: 'Official FudCake Rep',	  
+      warn: true,
+    },
+    {
+      id: 'ban_1bananobh5rat99qfgt1ptpieie5swmoth87thi74qgbfrij7dcgjiij94xr',
+      name: 'Official BananoRatPie Rep',
+      warn: true,
+    },
+    {
+      id: 'ban_1banbet955hwemgsqrb8afycd3nykaqaxsn7iaydcctfrwi3rbb36y17fbcb',
+      name: 'BananoBet Rep',
+      warn: true,
+    },
+    {
+      id: 'ban_1ka1ium4pfue3uxtntqsrib8mumxgazsjf58gidh1xeo5te3whsq8z476goo',
+      name: 'Kalium Rep',
+      warn: true,
+    },
+{
+      id: 'ban_1creepi89mp48wkyg5fktgap9j6165d8yz6g1fbe5pneinz3by9o54fuq63m',
+      name: ' creeper.banano.cc',
+      warn: false,
+    },
+{
+      id: 'ban_1tipbotgges3ss8pso6xf76gsyqnb69uwcxcyhouym67z7ofefy1jz7kepoy',
+      name: 'Banano-Tipbots',
+      warn: false,
+    },
+{
+      id: 'ban_1sebrep1mbkdtdb39nsouw5wkkk6o497wyrxtdp71sm878fxzo1kwbf9k79b',
+      name: '1sebrep1 (DE)',
+      warn: false,
+    },
+{
+      id: 'ban_1bestrep6gq14bt4bi7w446m9knc6matfad7qcii7edeb33iipooh46dotdz',
+      name: '1bestrep (CH)',
+      warn: false,
+    },
+{
+      id: 'ban_14z56meuqz6etgaik1ucsuyzcmp4aju73ziqkct3jdn3eqsgigihhkjitibz',
+      name: 'protecc iazid (FR)',
+      warn: false,
+    },
+  ];
 };
