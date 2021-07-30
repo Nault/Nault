d27b8a206a42acd915a78ac3a10460e0f35eabfa
Joohansson <2406720+Joohansson@users.noreply.github.com>
2021-07-30 11:49:54 +0200

    Removed NF reps from rep book Hardcoded warning

--- src/app/services/representative.service.ts
+++ src/app/services/representative.service.ts
@@ -14,6 +14,7 @@ export interface RepresentativeStatus {
   lowUptime: boolean;
   closing: boolean;
   markedToAvoid: boolean;
+  markedAsNF: boolean;
   trusted: boolean;
   changeRequired: boolean;
   warn: boolean;
@@ -103,6 +104,17 @@ export class RepresentativeService {
         ) {
           needsChange.push(rep);
       }
+
+      // Also check hardcoded NF reps
+      const nf = this.nfReps.find(bad => bad.id === rep.id);
+      if (nf) {
+        rep.status.markedAsNF = true;
+        rep.status.changeRequired = true;
+        rep.status.warn = true;
+        rep.statusText = 'alert';
+        rep.label = nf.name;
+        needsChange.push(rep);
+      }
     }
 
     this.changeableReps = needsChange;
@@ -145,6 +157,7 @@ export class RepresentativeService {
         lowUptime: false,
         closing: false,
         markedToAvoid: false,
+        markedAsNF: false,
         trusted: false,
         daysSinceLastVoted: 0,
         changeRequired: false,
@@ -441,46 +454,42 @@ export class RepresentativeService {
 
   // Default representatives list
   // tslint:disable-next-line:member-ordering
-  defaultRepresentatives = [
+  defaultRepresentatives = [];
+
+  // Bad representatives hardcoded to be avoided. Not visible in the user rep list
+  // tslint:disable-next-line:member-ordering
+  nfReps = [
     {
       id: 'nano_3arg3asgtigae3xckabaaewkx3bzsh7nwz7jkmjos79ihyaxwphhm6qgjps4',
       name: 'Nano Foundation #1',
-      warn: true,
     },
     {
       id: 'nano_1stofnrxuz3cai7ze75o174bpm7scwj9jn3nxsn8ntzg784jf1gzn1jjdkou',
       name: 'Nano Foundation #2',
-      warn: true,
     },
     {
       id: 'nano_1q3hqecaw15cjt7thbtxu3pbzr1eihtzzpzxguoc37bj1wc5ffoh7w74gi6p',
       name: 'Nano Foundation #3',
-      warn: true,
     },
     {
       id: 'nano_3dmtrrws3pocycmbqwawk6xs7446qxa36fcncush4s1pejk16ksbmakis78m',
       name: 'Nano Foundation #4',
-      warn: true,
     },
     {
       id: 'nano_3hd4ezdgsp15iemx7h81in7xz5tpxi43b6b41zn3qmwiuypankocw3awes5k',
       name: 'Nano Foundation #5',
-      warn: true,
     },
     {
       id: 'nano_1awsn43we17c1oshdru4azeqjz9wii41dy8npubm4rg11so7dx3jtqgoeahy',
       name: 'Nano Foundation #6',
-      warn: true,
     },
     {
       id: 'nano_1anrzcuwe64rwxzcco8dkhpyxpi8kd7zsjc1oeimpc3ppca4mrjtwnqposrs',
       name: 'Nano Foundation #7',
-      warn: true,
     },
     {
       id: 'nano_1hza3f7wiiqa7ig3jczyxj5yo86yegcmqk3criaz838j91sxcckpfhbhhra1',
       name: 'Nano Foundation #8',
-      warn: true,
     },
   ];
 
