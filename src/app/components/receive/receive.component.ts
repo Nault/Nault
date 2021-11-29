e3c85ecf2965f98c08bb5d4340d07151dc752286
BitDesert <11021604+BitDesert@users.noreply.github.com>
2021-11-29 22:36:42 +0100

    more receive

--- src/app/components/receive/receive.component.ts
+++ src/app/components/receive/receive.component.ts
@@ -135,7 +135,7 @@ export class ReceiveComponent implements OnInit, OnDestroy {
                 ),
                 destinationAddressBookName: (
                     this.addressBook.getAccountName(pendingBlock.account)
-                  || this.getAccountLabel(pendingBlock.account, 'Account')
+                  || this.getAccountLabel(pendingBlock.account, this.translocoService.translate('general.account'))
                 ),
                 isReceivable: true,
                 local_time_string: '',
@@ -245,7 +245,7 @@ export class ReceiveComponent implements OnInit, OnDestroy {
   onSelectedAccountChange(accountID) {
     this.selectedAccountAddressBookName = (
         this.addressBook.getAccountName(accountID)
-      || this.getAccountLabel(accountID, 'Account')
+      || this.getAccountLabel(accountID, this.translocoService.translate('general.account'))
     );
 
     this.changeQRAccount(accountID);
@@ -308,7 +308,7 @@ export class ReceiveComponent implements OnInit, OnDestroy {
     }
 
     if (this.walletService.walletIsLocked()) {
-      return this.notificationService.sendWarning(`Wallet must be unlocked`);
+      return this.notificationService.sendWarning(this.translocoService.translate('general.wallet-must-be-unlocked'));
     }
     receivableBlock.loading = true;
 
@@ -319,13 +319,13 @@ export class ReceiveComponent implements OnInit, OnDestroy {
       receivableBlock.received = true;
       this.mobileTransactionMenuModal.hide();
       this.notificationService.removeNotification('success-receive');
-      this.notificationService.sendSuccess(`Successfully received nano!`, { identifier: 'success-receive' });
+      this.notificationService.sendSuccess(this.translocoService.translate('receive.successfully-received-nano'), { identifier: 'success-receive' });
       // pending has been processed, can be removed from the list
       // list also updated with reloadBalances but not if called too fast
       this.walletService.removePendingBlock(receivableBlock.hash);
     } else {
       if (!this.walletService.isLedgerWallet()) {
-        this.notificationService.sendError(`There was a problem receiving the transaction, try manually!`, {length: 10000});
+        this.notificationService.sendError(this.translocoService.translate('receive.there-was-a-problem-receiving-the-transaction-try-manually'), {length: 10000});
       }
     }
 
@@ -336,7 +336,7 @@ export class ReceiveComponent implements OnInit, OnDestroy {
 
   copied() {
     this.notificationService.removeNotification('success-copied');
-    this.notificationService.sendSuccess(`Successfully copied to clipboard!`, { identifier: 'success-copied' });
+    this.notificationService.sendSuccess(this.translocoService.translate('general.successfully-copied-to-clipboard'), { identifier: 'success-copied' });
   }
 
   copiedAccountAddress() {
