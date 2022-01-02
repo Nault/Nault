2246131694980dde6a6feec4f7a11545bf3ae56d
BitDesert <11021604+BitDesert@users.noreply.github.com>
2022-01-02 21:50:30 +0100

    angular 13

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -13,12 +13,6 @@ import '@angular/localize/init';
 // (window as any).global = window;
 global.Buffer = global.Buffer || require('buffer').Buffer;
 
-/**
- * Required to support Web Animations `@angular/platform-browser/animations`.
- * Needed for: All but Chrome, Firefox and Opera. http://caniuse.com/#feat=web-animation
- **/
-// import 'web-animations-js';  // Run `npm install --save web-animations-js`.
-
 
 
 /***************************************************************************************************
