90f6a0844d78a7d02169c96a64aae9b3176b270a
George Mamar <george@mamar.dev>
2022-06-24 12:16:30 +0200

    disable back-navigation while modal is shown

--- src/app/directives/hide-modal.directive.ts
+++ src/app/directives/hide-modal.directive.ts
@@ -1,24 +1,38 @@
 import { Directive, ElementRef, HostListener } from '@angular/core';
 
 /**
- * auto hides modals whenever the user navigates to previous page (clicking back button, or by keyboard..)
- * @requires id on HTML element
+ * Auto closes modals whenever the user navigates to previous page (clicking back button or by keyboard) WITHOUT navigating backwards
+ * @requires id on HTML element that has 'uk-modal' attribute
  */
 @Directive({
   selector: '[appHideModal]'
 })
 export class HideModalDirective {
+  UIkit = window['UIkit'];
+  modal = this.UIkit?.modal(`#${this.element.nativeElement.id}`);
 
-    constructor(private element: ElementRef) { }
+  constructor(private element: ElementRef) {
 
-    @HostListener('window:popstate', ['$event'])
-    onPopState(event) {
-      const UIkit = window['UIkit'];
+    document.addEventListener('shown', () => {
+      const cancelBtn = document.querySelector(`#${this.element.nativeElement.id} .uk-modal-close`);
 
-      if (UIkit && this.element.nativeElement.id) {
-        const modal = UIkit.modal(`#${this.element.nativeElement.id}`);
-        if (typeof modal?.hide === 'function')
-          modal.hide();
+      if (this.modal?.isToggled()) {
+        // create a fake state when showing a modal, so that navigating backwards only closes the modal without changing history
+        history.pushState(null, null);
+        // removes fake state when canceling the modal
+        cancelBtn?.addEventListener('click', this.goBack);
       }
+    });
+  }
+
+  @HostListener('window:popstate', ['$event'])
+  onPopState(event) {
+    if (this.modal?.isToggled() && typeof this.modal?.hide === 'function') {
+      this.modal.hide();
     }
+  }
+
+  private goBack(event) {
+    history.back();
+  }
 }
