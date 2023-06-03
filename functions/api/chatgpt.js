6f01b252ec00592c5832ce7f00e92083d4cb4e48
Esteban <esteban@fwd.dev>
2023-06-03 13:01:51 -0400

    fix

--- functions/api/chatgpt.js
+++ functions/api/chatgpt.js
@@ -24,7 +24,7 @@ export async function onRequestGet(ctx) {
             "messages": req.body.messages,
             // "temperature": 0.7,
             // "frequency_penalty": 0.5
-          }, { headers: { 'Authorization': `Bearer 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`, } })
+          }, { headers: { 'Authorization': , } })
           
 
   const response = await fetch('https://nano.to/known.json', init);
